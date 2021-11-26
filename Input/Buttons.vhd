2752f94b340e8f149882d2d47bcad6d4ce04486c
paperbin <84015800+paperbin@users.noreply.github.com>
2021-11-26 10:50:24 +0100

    Add files via upload
    Gefixt want EPO-3 test board heeft maar 4 knoppen

--- Input/Buttons.vhd
+++ Input/Buttons.vhd
@@ -4,14 +4,11 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity buttons is
 port(   
-	button_c : in std_logic;
-	button_d : in std_logic;
-	button_e : in std_logic;
-	button_f : in std_logic;
-	button_g : in std_logic;
-	button_a : in std_logic;
-        button_b : in std_logic; 
-	button_k : in std_logic;
+	KEY0 : in std_logic;
+	KEY1 : in std_logic;
+	KEY2 : in std_logic;
+	KEY3 : in std_logic;
+	SW0  : in std_logic;
 	notes_OUT : out std_logic_vector(2 downto 0)
      );
 end buttons;
@@ -20,24 +17,28 @@ architecture behaviour of buttons is
 
 
 begin
-	process(button_c,button_d,button_e,button_f,button_g,button_a,button_b,button_k)
+	process(SW0)
 	begin
-		if button_c = '1' then
-			notes_OUT <= "000";
-		elsif button_d = '1' then
-			notes_OUT <= "001";
-		elsif button_e = '1' then
-			notes_OUT <= "010";
-		elsif button_f = '1' then
-			notes_OUT <= "100";
-		elsif button_g = '1' then
-			notes_OUT <= "110";
-		elsif button_a = '1' then
-			notes_OUT <= "101";
-		elsif button_b = '1' then
-			notes_OUT <= "011";
-		elsif button_k = '1' then
-			notes_OUT <= "111";
+		if SW0 = '0' then
+			if KEY0 = '1' then
+				notes_OUT <= "000";
+			elsif KEY1 = '1' then
+				notes_OUT <= "001";
+			elsif KEY2 = '1' then
+				notes_OUT <= "010";
+			elsif KEY3 = '1' then
+				notes_OUT <= "100";
+			end if;
+		elsif SW0 = '1' then
+			if KEY0 = '1' then
+				notes_OUT <= "110";
+			elsif KEY1 = '1' then
+				notes_OUT <= "101";
+			elsif KEY2 = '1' then
+				notes_OUT <= "011";
+			elsif KEY3 = '1' then
+				notes_OUT <= "111";
+			end if;
 		end if;
 	end process;
 end architecture;
