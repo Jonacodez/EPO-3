aa1a70ff8402346497f55ad4c1ede1e86b471dc3
paperbin <84015800+paperbin@users.noreply.github.com>
2021-11-26 09:06:51 +0100

    Update input_arduino.vhd

--- Input/input_arduino.vhd
+++ Input/input_arduino.vhd
@@ -18,21 +18,21 @@ threebits <= notesA_IN (4 downto 2); --remove last 2 bits from arduino
 	process(notesA_IN)
 	begin
 		if(threebits = "000") then
-			notesA_OUT <= 's';
-		elsif(threebits = "001") then
 			notesA_OUT <= 'c';
-		elsif(threebits = "010") then
-			notesA_OUT <= 'j';
-		elsif(threebits = "100") then
+		elsif(threebits = "001") then
 			notesA_OUT <= 'd';
-		elsif(threebits = "01100") then
-			notesA_OUT <= 'k';
-		elsif(threebits = "101") then
+		elsif(threebits = "010") then
 			notesA_OUT <= 'e';
-		elsif(threebits = "110") then
+		elsif(threebits = "100") then
 			notesA_OUT <= 'f';
-		elsif(threebits = "111") then
+		elsif(threebits = "01100") then
 			notesA_OUT <= 'g';
+		elsif(threebits = "101") then
+			notesA_OUT <= 'a';
+		elsif(threebits = "110") then
+			notesA_OUT <= 'b';
+		elsif(threebits = "111") then
+			notesA_OUT <= 'k';
 		end if;
 	end process;  
 end architecture;
