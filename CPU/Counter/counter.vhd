7e22340ac3155c6b0058064c88cf41a05e684eca
Turko61 <94609205+Turko61@users.noreply.github.com>
2021-12-20 11:19:03 +0100

    Add files via upload

--- CPU/Counter/counter.vhd
+++ CPU/Counter/counter.vhd
@@ -1,14 +1,11 @@
 library IEEE;
 use IEEE.std_logic_1164.ALL;
 use ieee.numeric_std.all;
-
 entity counter is
-   port(clk    : in  std_logic;
-        reset  : in  std_logic;
-        enable : in  std_logic;
-	notes_OUT : in std_logic_vector( 4 downto 0);
-	count_out  : out std_logic
-	
-);
+   port(clk        : in  std_logic;
+        reset      : in  std_logic;
+        succes_bit : in  std_logic;
+        notes_OUT  : in  std_logic_vector(4 downto 0);
+        count_out  : out std_logic);
 end counter;
 
