33cd6fac064481b8721b69d07d4e658700ddbf7e
skadoorn <93406848+skadoorn@users.noreply.github.com>
2021-12-20 11:06:12 +0100

    Add files via upload

--- skip en rob/com_com-behv.vhd
+++ skip en rob/com_com-behv.vhd
@@ -9,19 +9,19 @@ port	(clk	: in std_logic;
 	clk2	: out std_logic);
 end component;
 
-type states is (loading, shifting1, shifting2, sel_state, sel_state_wate, res_state);
+type states is (loading, shifting1, shifting2, sel_state, load_state_wate, res_state);
 signal state: states; 
 signal new_state: states;
-signal a_in, a_out, a_out_new: std_logic_vector(4 downto 0);
+signal a_out, a_out_new: std_logic_vector(4 downto 0);
 signal con_d: std_logic;
 signal ss, ss_new: std_logic_vector(2 downto 0);
-signal sh, res_tb, suc: std_logic;
+signal res_tb: std_logic;
 
 begin
 
 l1: com_timebase port map (clk, res_tb, handshake);
 
-	stijger: process(clk)
+	stijger: process(clk, reset)
 	begin
 	if reset = '1' then 
 		state <= res_state;
@@ -30,84 +30,77 @@ l1: com_timebase port map (clk, res_tb, handshake);
 			state <= new_state;
 			ss <= ss_new;
 			a_out <= a_out_new;
+			con_d <= confirm;
 		end if;
 	end if;
 	end process;
 	
-	fsm: process(clk)
+	fsm: process(state, reset, mat_in, confirm, ard_in, succes, end_bit, a_out, ss, con_d)
 	begin
-	if rising_edge(clk) then
+
 		case state is
 			when res_state => 
 				res_tb <= '1';
-				sh <= '0';
+				shift <= '0';
 				ss_new <= "000";
 				a_out_new <= "00000";
 				if reset = '0' then 
 					new_state <= sel_state;
 				else
-					new_state <= sel_state_wate;
+					new_state <= state;
 				end if;
 			when sel_state =>
 				a_out_new <= "00000";
 				ss_new <= mat_in;
-				sh <= '0';
+				shift <= '0';
 				if mat_in = "000" then
 					new_state <= state;
 				else
-					new_state <= sel_state_wate;
+					new_state <= load_state_wate;
 				end if;
-			when sel_state_wate =>
-				a_out_new <= "00000";
+			when load_state_wate =>
+				res_tb <= '0';
 				ss_new <= mat_in;
-				sh <= '0';
-				con_d <= confirm;
+				shift <= '0';
 				if con_d /= confirm then
 					new_state <= loading;
-				else new_state <= state;
-				end if;
-			when loading => 
-				res_tb <= '0';
-				ss_new <= ss;
-				sh <= '0';
-				con_d <= confirm;
-				if con_d /= confirm then
-					a_out_new <= a_in;
-					sh <= '1';
 				else
 					a_out_new <= a_out;
+					new_state <= state;
 				end if;
-				if a_in = "00000" then
+			when loading =>
+				shift <= '1';
+				a_out_new <= ard_in;
+				if ard_in = "00000" then
 					new_state <= shifting1;
-				else new_state <= state;
+				else 
+					new_state <= load_state_wate;
 				end if;
+				ss_new <= mat_in;
+				res_tb <= '0';
 			when shifting1 =>
 				res_tb <= '1';
 				ss_new <= ss;
-				sh <= '0';
 				a_out_new <= a_out;
-				if suc = '1' then
-					sh <= '1';
+				if succes = '1' then
+					shift <= '1';
 					new_state <= shifting2;
 				elsif end_bit = '1' then
-					new_state <= sel_state_wate;
+					new_state <= sel_state;
+					shift <= '0';
 				else
 					new_state <= state;
+					shift <= '0';
 				end if;
 			when shifting2 =>
 				new_state <= shifting1;
 				ss_new <= ss;
-				sh <= '1';
+				shift <= '0';
 				a_out_new <= a_out;
-				
 		end case;
-	end if;
 	end process;
 
-suc <= succes;
-a_in <= ard_in;
-arr_out <= a_out;
+arr_out <= a_out_new;
 song_sel <= ss;
-shift <= sh;
 
 end behv;
--- skip en rob/com_com.vhd
+++ skip en rob/com_com.vhd
@@ -1,108 +1,17 @@
-
 library IEEE;
 use IEEE.std_logic_1164.ALL;
 
 entity com_com is
-port	(ard_in	: in std_logic_vector(4 downto 0);
-	confirm : in std_logic;
-	mat_in  : in std_logic_vector(2 downto 0);
-	succes	: in std_logic;
-	clk	: in std_logic;
-	reset	: in std_logic;
-	end_bit : in std_logic;
-	arr_out : out std_logic_vector(4 downto 0);
-	song_sel : out std_logic_vector(2 downto 0);
-	handshake : out std_logic;
-	shift	: out std_logic);
-end entity;
-
-architecture behv of com_com is
-
-component com_timebase is
-port	(clk	: in std_logic;
-	reset	: in std_logic;
-	clk2	: out std_logic);
-end component;
-
-type states is (loading, shifting1, shifting2, sel_state, sel_state_wate, res_state);
-signal state: states; 
-signal new_state: states:= sel_state;
-signal a_in, a_out: std_logic_vector(4 downto 0);
-signal con_d: std_logic;
-signal ss: std_logic_vector(2 downto 0);
-signal sh, nclk, res_tb, suc: std_logic;
-
-begin
-
-l1: com_timebase port map (clk, res_tb, nclk);
-
-	stijger: process(clk)
-	begin
-	if reset = '1' then 
-		state <= res_state;
-	else
-		if rising_edge(clk) then
-			state <= new_state;
-		end if;
-	end if;
-	end process;
-	
-	fsm: process(clk)
-	begin
-	if rising_edge(clk) then
-		case state is
-			when res_state => 
-				res_tb <= '1';
-				ss <= "000";
-				if reset = '0' then new_state <= sel_state;
-				end if;
-			when sel_state =>
-				ss <= mat_in;
-				if mat_in = "000" then
-					new_state <= state;
-				else
-					new_state <= sel_state_wate;
-				end if;
-			when sel_state_wate =>
-				con_d <= confirm;
-				if con_d /= confirm then
-					new_state <= loading;
-				end if;
-			when loading => 
-				res_tb <= '0';
-				sh <= '0';
-				con_d <= confirm;
-				if con_d /= confirm then
-					a_out <= a_in;
-					sh <= '1';
-				end if;
-				if a_in = "00000" then
-					new_state <= shifting1;
-				end if;
-			when shifting1 =>
-				res_tb <= '1';
-				sh <= '0';
-				if suc = '1' then
-					sh <= '1';
-					new_state <= shifting2;
-				elsif end_bit = '1' then
-					new_state <= sel_state_wate;
-				else
-					new_state <= state;
-				end if;
-			when shifting2 =>
-				new_state <= shifting1;
-				
-		end case;
-	end if;
-	end process;
-
-suc <= succes;
-a_in <= ard_in;
-arr_out <= a_out;
-song_sel <= ss;
-shift <= sh;
-handshake <= nclk;
-
-end behv;
+   port(ard_in    : in  std_logic_vector(4 downto 0);
+        confirm   : in  std_logic;
+        mat_in    : in  std_logic_vector(2 downto 0);
+        succes    : in  std_logic;
+        clk       : in  std_logic;
+        reset     : in  std_logic;
+        end_bit   : in  std_logic;
+        arr_out   : out std_logic_vector(4 downto 0);
+        song_sel  : out std_logic_vector(2 downto 0);
+        handshake : out std_logic;
+        shift     : out std_logic);
+end com_com;
 
