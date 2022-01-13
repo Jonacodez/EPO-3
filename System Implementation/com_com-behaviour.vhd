d8449495395d06e082d972ba87519406535abc5a
skadoorn <93406848+skadoorn@users.noreply.github.com>
2022-01-13 15:17:39 +0100

    Update com_com-behaviour.vhd

--- System Implementation/com_com-behaviour.vhd
+++ System Implementation/com_com-behaviour.vhd
@@ -14,7 +14,7 @@ signal state: states;
 signal new_state: states;
 signal a_out, a_out_new: std_logic_vector(4 downto 0);
 signal con_d: std_logic;
-signal ss, ss_new: std_logic_vector(2 downto 0);
+signal ss: std_logic_vector(2 downto 0);
 signal res_tb: std_logic;
 
 begin
@@ -28,7 +28,6 @@ l1: com_timebase port map (clk, res_tb, handshake);
 			state <= res_state;
 		else
 			state <= new_state;
-			ss <= ss_new;
 			a_out <= a_out_new;
 			con_d <= confirm;
 		end if;
@@ -42,7 +41,7 @@ l1: com_timebase port map (clk, res_tb, handshake);
 			when res_state => 
 				res_tb <= '1';
 				shift <= '0';
-				ss_new <= "000";
+				ss <= "000";
 				a_out_new <= "00000";
 				if reset = '0' then 
 					new_state <= sel_state;
@@ -51,8 +50,9 @@ l1: com_timebase port map (clk, res_tb, handshake);
 				end if;
 			when sel_state =>
 				a_out_new <= "00000";
-				ss_new <= mat_in;
+				ss <= mat_in;
 				shift <= '0';
+				res_tb <= '0';
 				if mat_in = "000" then
 					new_state <= state;
 				else
@@ -60,27 +60,27 @@ l1: com_timebase port map (clk, res_tb, handshake);
 				end if;
 			when load_state_wate =>
 				res_tb <= '0';
-				ss_new <= mat_in;
+				ss <= mat_in;
 				shift <= '0';
+				a_out_new <= a_out;
 				if con_d /= confirm then
 					new_state <= loading;
 				else
-					a_out_new <= a_out;
 					new_state <= state;
 				end if;
 			when loading =>
 				shift <= '1';
 				a_out_new <= ard_in;
+				ss <= "00000";
+				res_tb <= '0';
 				if ard_in = "00000" then
 					new_state <= shifting1;
 				else 
 					new_state <= load_state_wate;
 				end if;
-				ss_new <= mat_in;
-				res_tb <= '0';
 			when shifting1 =>
 				res_tb <= '1';
-				ss_new <= ss;
+				ss <= "00000";
 				a_out_new <= a_out;
 				if succes = '1' then
 					shift <= '1';
@@ -93,8 +93,9 @@ l1: com_timebase port map (clk, res_tb, handshake);
 					shift <= '0';
 				end if;
 			when shifting2 =>
+				res_tb <= '1';
 				new_state <= shifting1;
-				ss_new <= ss;
+				ss <= "00000";
 				shift <= '0';
 				a_out_new <= a_out;
 		end case;
