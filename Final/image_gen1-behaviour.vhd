30460c0011a2f9ebb07c8121de1337300b4c2275
paperbin <84015800+paperbin@users.noreply.github.com>
2022-01-16 22:16:44 +0100

    Add files via upload

--- Final/image_gen1-behaviour.vhd
+++ Final/image_gen1-behaviour.vhd
@@ -11,13 +11,13 @@ architecture behaviour of image_gen1 is
 	signal r_int, g_int, b_int		: std_logic_vector (50 downto 0):= (others => '0');
 	type integer_array is array(0 to 23) of integer;
 	signal shift				: integer_array;
-	signal init				: std_logic_Vector (23 downto 0):= (others => '0');
+	signal init				: std_logic_Vector (26 downto 0):= (others => '0');
 	signal shift_speed			: integer;
 
 begin
 Hcount_int <= to_integer(unsigned(Hcount));
 Vcount_int <= to_integer(unsigned(Vcount));
-shift_speed <= 10;
+shift_speed <= 5;
 
 	
 
@@ -29,7 +29,7 @@ shift_speed <= 10;
 		b_int(1 downto 0) <= "00";
 		if(display_enable = '1') then
 			if(Vcount_int >= v2 and Vcount_int <= v3) then
-				if((Hcount_int >= h2 and Hcount_int <= h2 + 2) or (Hcount_int >= h2 + 79 and Hcount_int <= h2 + 80) or (Hcount_int >= h2 + 159 and Hcount_int <= h2 + 160) or (Hcount_int >= h2 + 239 and Hcount_int <= h2 + 240) or (Hcount_int >= h2 + 319 and Hcount_int <= h2 + 320) or (Hcount_int >= h2 + 399 and Hcount_int <= h2 + 400) or (Hcount_int >= h2 + 479 and Hcount_int <= h2 + 480) or (Hcount_int >= h2 + 559 and Hcount_int <= h2 + 560) or (Hcount_int >= h2 + 639 and Hcount_int <= h2 + 640)) then 
+				if	((Hcount_int >= h2 and Hcount_int <= h2 + 2) or (Hcount_int >= h2 + 79 and Hcount_int <= h2 + 80) or (Hcount_int >= h2 + 159 and Hcount_int <= h2 + 160) or (Hcount_int >= h2 + 239 and Hcount_int <= h2 + 240) or (Hcount_int >= h2 + 319 and Hcount_int <= h2 + 320) or (Hcount_int >= h2 + 399 and Hcount_int <= h2 + 400) or (Hcount_int >= h2 + 479 and Hcount_int <= h2 + 480) or (Hcount_int >= h2 + 559 and Hcount_int <= h2 + 560) or (Hcount_int >= h2 + 639 and Hcount_int <= h2 + 640)) then 
 					r_int(1 downto 0) <= "11";
 					g_int(1 downto 0) <= "11";
 					b_int(1 downto 0) <= "11";
@@ -42,6 +42,7 @@ shift_speed <= 10;
 			end if;
 		end if;
 	end process;
+
 
 
 	notecontrol:
@@ -57,7 +58,7 @@ shift_speed <= 10;
 		elsif(input(1 downto 0) = "11") then
 			init(2) <= '1';
 		end if;
-	elsif(input(4 downto 2) = "001" and note_confirm = '1' and rising_edge(pixel_clk)) then --note1
+	elsif(input(4 downto 2) = "001" and input(1 downto 0) /= "00" and note_confirm = '1' and rising_edge(pixel_clk)) then --note1
 		if(input(1 downto 0) = "01") then
 			init(3) <= '1';
 		elsif(input(1 downto 0) = "10") then
@@ -65,7 +66,7 @@ shift_speed <= 10;
 		elsif(input(1 downto 0) = "11") then
 			init(5) <= '1';
 		end if;
-	elsif(input(4 downto 2) = "010" and note_confirm = '1' and rising_edge(pixel_clk)) then --note2
+	elsif(input(4 downto 2) = "010" and input(1 downto 0) /= "00" and note_confirm = '1' and rising_edge(pixel_clk)) then --note2
 		if(input(1 downto 0) = "01") then
 			init(6) <= '1';
 		elsif(input(1 downto 0) = "10") then
@@ -73,7 +74,7 @@ shift_speed <= 10;
 		elsif(input(1 downto 0) = "11") then
 			init(8) <= '1';
 		end if;
-	elsif(input(4 downto 2) = "011" and note_confirm = '1' and rising_edge(pixel_clk)) then --note3
+	elsif(input(4 downto 2) = "011" and input(1 downto 0) /= "00" and note_confirm = '1' and rising_edge(pixel_clk)) then --note3
 		if(input(1 downto 0) = "01") then
 			init(9) <= '1';
 		elsif(input(1 downto 0) = "10") then
@@ -81,7 +82,7 @@ shift_speed <= 10;
 		elsif(input(1 downto 0) = "11") then
 			init(11) <= '1';
 		end if;
-	elsif(input(4 downto 2) = "100" and note_confirm = '1' and rising_edge(pixel_clk)) then --note4
+	elsif(input(4 downto 2) = "100" and input(1 downto 0) /= "00" and note_confirm = '1' and rising_edge(pixel_clk)) then --note4
 		if(input(1 downto 0) = "01") then
 			init(12) <= '1';
 		elsif(input(1 downto 0) = "10") then
@@ -89,7 +90,7 @@ shift_speed <= 10;
 		elsif(input(1 downto 0) = "11") then
 			init(14) <= '1';
 		end if;
-	elsif(input(4 downto 2) = "101" and note_confirm = '1' and rising_edge(pixel_clk)) then --note5
+	elsif(input(4 downto 2) = "101" and input(1 downto 0) /= "00" and note_confirm = '1' and rising_edge(pixel_clk)) then --note5
 		if(input(1 downto 0) = "01") then
 			init(15) <= '1';
 		elsif(input(1 downto 0) = "10") then
@@ -97,7 +98,7 @@ shift_speed <= 10;
 		elsif(input(1 downto 0) = "11") then
 			init(17) <= '1';
 		end if;
-	elsif(input(4 downto 2) = "110" and note_confirm = '1' and rising_edge(pixel_clk)) then --note6
+	elsif(input(4 downto 2) = "110" and input(1 downto 0) /= "00" and note_confirm = '1' and rising_edge(pixel_clk)) then --note6
 		if(input(1 downto 0) = "01") then
 			init(18) <= '1';
 		elsif(input(1 downto 0) = "10") then
@@ -105,7 +106,7 @@ shift_speed <= 10;
 		elsif(input(1 downto 0) = "11") then
 			init(20) <= '1';
 		end if;
-	elsif(input(4 downto 2) = "111" and note_confirm = '1' and rising_edge(pixel_clk)) then --note7
+	elsif(input(4 downto 2) = "111" and input(1 downto 0) /= "00" and note_confirm = '1' and rising_edge(pixel_clk)) then --note7
 		if(input(1 downto 0) = "01") then
 			init(21) <= '1';
 		elsif(input(1 downto 0) = "10") then
@@ -189,137 +190,25 @@ shift_speed <= 10;
 	end process;
 
 	note0: 
-	process(pixel_clk, reset, r_int, g_int, b_int, shift)
+	process(pixel_clk, reset, r_int, g_int, b_int, shift, Vcount_int, Hcount_int,init,display_enable)
 	begin
 
-	r_int <= r_int;
-	g_int <= g_int;
-	b_int <= b_int;
+   r_int(48 downto 2) <= r_int(48 downto 2);
+   g_int(48 downto 2) <= g_int(48 downto 2);
+   b_int(48 downto 2) <= b_int(48 downto 2);
 	shift <= shift;
 	if(reset = '1') then --reset
 		shift <= (others => 0);
-	elsif(rising_edge(pixel_clk)) then
-		if(Vcount_int = 0 and Hcount_int = 0  and init(0) = '1' and shift(0) /= 480 and reset = '0' and rising_edge(pixel_clk)) then --shift speed
-			shift(0) <= shift(0) + shift_speed;
-		elsif(shift(0) > 480) then
-			shift(0) <= 0;
-		end if;
-		if(Vcount_int = 0 and Hcount_int = 0  and init(1) = '1' and shift(1) /= 480 and reset = '0' and rising_edge(pixel_clk)) then
-			shift(1) <= shift(1) + shift_speed;
-		elsif(shift(1) > 480) then
-			shift(1) <= 0;
-		end if;
-		if(Vcount_int = 0 and Hcount_int = 0  and init(2) = '1' and shift(2) /= 480 and reset = '0' and rising_edge(pixel_clk)) then
-			shift(2) <= shift(2) + shift_speed;
-		elsif(shift(2) > 480) then
-			shift(2) <= 0;
-		end if;
-		if(Vcount_int = 0 and Hcount_int = 0  and init(3) = '1' and shift(3) /= 480 and reset = '0' and rising_edge(pixel_clk)) then
-			shift(3) <= shift(3) + shift_speed;
-		elsif(shift(3) > 480) then
-			shift(3) <= 0;
-		end if;
-		if(Vcount_int = 0 and Hcount_int = 0  and init(4) = '1' and shift(4) /= 480 and reset = '0' and rising_edge(pixel_clk)) then
-			shift(4) <= shift(4) + shift_speed;
-		elsif(shift(4) > 480) then
-			shift(4) <= 0;
-		end if;
-		if(Vcount_int = 0 and Hcount_int = 0  and init(5) = '1' and shift(5) /= 480 and reset = '0' and rising_edge(pixel_clk)) then
-			shift(5) <= shift(5) + shift_speed;
-		elsif(shift(5) > 480) then
-			shift(5) <= 0;
-		end if;
-		if(Vcount_int = 0 and Hcount_int = 0  and init(6) = '1' and shift(6) /= 480 and reset = '0' and rising_edge(pixel_clk)) then
-			shift(6) <= shift(6) + shift_speed;
-		elsif(shift(6) > 480) then
-			shift(6) <= 0;
-		end if;
-		if(Vcount_int = 0 and Hcount_int = 0  and init(7) = '1' and shift(7) /= 480 and reset = '0' and rising_edge(pixel_clk)) then
-			shift(7) <= shift(7) + shift_speed;
-		elsif(shift(7) > 480) then
-			shift(7) <= 0;
-		end if;
-		if(Vcount_int = 0 and Hcount_int = 0  and init(8) = '1' and shift(8) /= 480 and reset = '0' and rising_edge(pixel_clk)) then
-			shift(8) <= shift(8) + shift_speed;
-		elsif(shift(8) > 480) then
-			shift(8) <= 0;
-		end if;
-		if(Vcount_int = 0 and Hcount_int = 0  and init(9) = '1' and shift(9) /= 480 and reset = '0' and rising_edge(pixel_clk)) then
-			shift(9) <= shift(9) + shift_speed;
-		elsif(shift(9) > 480) then
-			shift(9) <= 0;
-		end if;
-		if(Vcount_int = 0 and Hcount_int = 0  and init(10) = '1' and shift(10) /= 480 and reset = '0' and rising_edge(pixel_clk)) then
-			shift(10) <= shift(10) + shift_speed;
-		elsif(shift(10) > 480) then
-			shift(10) <= 0;
-		end if;
-		if(Vcount_int = 0 and Hcount_int = 0  and init(11) = '1' and shift(11) /= 480 and reset = '0' and rising_edge(pixel_clk)) then
-			shift(11) <= shift(11) + shift_speed;
-		elsif(shift(11) > 480) then
-			shift(11) <= 0;
-		end if;
-		if(Vcount_int = 0 and Hcount_int = 0  and init(12) = '1' and shift(12) /= 480 and reset = '0' and rising_edge(pixel_clk)) then
-			shift(12) <= shift(12) + shift_speed;
-		elsif(shift(12) > 480) then
-			shift(12) <= 0;
-		end if;
-		if(Vcount_int = 0 and Hcount_int = 0  and init(13) = '1' and shift(13) /= 480 and reset = '0' and rising_edge(pixel_clk)) then
-			shift(13) <= shift(13) + shift_speed;
-		elsif(shift(13) > 480) then
-			shift(13) <= 0;
-		end if;
-		if(Vcount_int = 0 and Hcount_int = 0  and init(12) = '1' and shift(14) /= 480 and reset = '0' and rising_edge(pixel_clk)) then
-			shift(14) <= shift(14) + shift_speed;
-		elsif(shift(14) > 480) then
-			shift(14) <= 0;
-		end if;
-		if(Vcount_int = 0 and Hcount_int = 0  and init(15) = '1' and shift(15) /= 480 and reset = '0' and rising_edge(pixel_clk)) then
-			shift(15) <= shift(15) + shift_speed;
-		elsif(shift(15) > 480) then
-			shift(15) <= 0;
-		end if;
-		if(Vcount_int = 0 and Hcount_int = 0  and init(12) = '1' and shift(16) /= 480 and reset = '0' and rising_edge(pixel_clk)) then
-			shift(16) <= shift(16) + shift_speed;
-		elsif(shift(16) > 480) then
-			shift(16) <= 0;
-		end if;
-		if(Vcount_int = 0 and Hcount_int = 0  and init(17) = '1' and shift(17) /= 480 and reset = '0' and rising_edge(pixel_clk)) then
-			shift(17) <= shift(17) + shift_speed;
-		elsif(shift(17) > 480) then
-			shift(17) <= 0;	if(reset = '1' and rising_edge(pixel_clk)) then --reset
-		end if;
-		if(Vcount_int = 0 and Hcount_int = 0  and init(18) = '1' and shift(18) /= 480 and reset = '0' and rising_edge(pixel_clk)) then
-			shift(18) <= shift(18) + shift_speed;
-		elsif(shift(18) > 480) then
-			shift(18) <= 0;
-		end if;
-		if(Vcount_int = 0 and Hcount_int = 0  and init(19) = '1' and shift(19) /= 480 and reset = '0' and rising_edge(pixel_clk)) then
-			shift(19) <= shift(19) + shift_speed;
-		elsif(shift(19) > 480) then
-			shift(19) <= 0;
-		end if;
-		if(Vcount_int = 0 and Hcount_int = 0  and init(20) = '1' and shift(20) /= 480 and reset = '0' and rising_edge(pixel_clk)) then
-			shift(20) <= shift(20) + shift_speed;
-		elsif(shift(20) > 480) then
-			shift(20) <= 0;
-		end if;
-		if(Vcount_int = 0 and Hcount_int = 0  and init(21) = '1' and shift(21) /= 480 and reset = '0' and rising_edge(pixel_clk)) then
-			shift(21) <= shift(21) + shift_speed;
-		elsif(shift(21) > 480) then
-			shift(21) <= 0;
-		end if;
-		if(Vcount_int = 0 and Hcount_int = 0  and init(22) = '1' and shift(22) /= 480 and reset = '0' and rising_edge(pixel_clk)) then
-			shift(22) <= shift(22) + shift_speed;
-		elsif(shift(22) > 480) then
-			shift(22) <= 0;
-		end if;
-		if(Vcount_int = 0 and Hcount_int = 0  and init(23) = '1' and shift(23) /= 480 and reset = '0' and rising_edge(pixel_clk)) then
-			shift(23) <= shift(23) + shift_speed;
-		elsif(shift(23) > 480) then
-			shift(23) <= 0;
-		end if;
+	elsif(rising_edge(pixel_clk) and reset = '0') then
+		for i in 0 to 23 loop
+			if(Vcount_int = 0 and Hcount_int = 0  and init(i) = '1' and shift(i) /= 480) then
+				shift(i) <= shift(i) + shift_speed;
+			elsif(init(i) = '0') then
+				shift(i) <= 0;
+			end if;
+		end loop;
 	end if;
+
 	if(display_enable = '1' and Hcount_int >= note1_left and Hcount_int <= note1_right) then -- note1	
 		if(Vcount_int >= block1_top + shift(0) and Vcount_int <= block1_bottom + shift(0) and init(0) = '1') then
 			r_int(3 downto 2) <= "11";
@@ -348,6 +237,10 @@ shift_speed <= 10;
 			g_int(7 downto 6) <= "00";
 			b_int(7 downto 6) <= "00";
 		end if;
+	else
+		r_int(7 downto 2) <= (others => '0');
+		g_int(7 downto 2) <= (others => '0');
+		b_int(7 downto 2) <= (others => '0');
 	end if;
 	if(display_enable = '1' and Hcount_int >= note2_left and Hcount_int <= note2_right) then	
 		if(Vcount_int >= block1_top + shift(3) and Vcount_int <= block1_bottom + shift(3) and init(3) = '1') then
@@ -377,6 +270,10 @@ shift_speed <= 10;
 			g_int(13 downto 12) <= "00";
 			b_int(13 downto 12) <= "00";
 		end if;
+	else
+		r_int(13 downto 8) <= (others => '0');
+		g_int(13 downto 8) <= (others => '0');
+		b_int(13 downto 8) <= (others => '0');
 	end if;
 	if(display_enable = '1' and Hcount_int >= note3_left and Hcount_int <= note3_right) then	
 		if(Vcount_int >= block1_top + shift(6) and Vcount_int <= block1_bottom + shift(6) and init(6) = '1') then
@@ -406,7 +303,11 @@ shift_speed <= 10;
 			g_int(19 downto 18) <= "00";
 			b_int(19 downto 18) <= "00";
 		end if;
-	end if;
+	else
+		r_int(19 downto 14) <= (others => '0');
+		g_int(19 downto 14) <= (others => '0');
+		b_int(19 downto 14) <= (others => '0');
+	end if;	
 	if(display_enable = '1' and Hcount_int >= note4_left and Hcount_int <= note4_right) then	
 		if(Vcount_int >= block1_top + shift(9) and Vcount_int <= block1_bottom + shift(9) and init(9) = '1') then
 			r_int(21 downto 20) <= "11";
@@ -435,6 +336,10 @@ shift_speed <= 10;
 			g_int(25 downto 24) <= "00";
 			b_int(25 downto 24) <= "00";
 		end if;
+	else
+		r_int(25 downto 20) <= (others => '0');
+		g_int(25 downto 20) <= (others => '0');
+		b_int(25 downto 20) <= (others => '0');
 	end if;
 	if(display_enable = '1' and Hcount_int >= note5_left and Hcount_int <= note5_right) then -- note1	
 		if(Vcount_int >= block1_top + shift(12) and Vcount_int <= block1_bottom + shift(12) and init(12) = '1') then
@@ -464,6 +369,10 @@ shift_speed <= 10;
 			g_int(31 downto 30) <= "00";
 			b_int(31 downto 30) <= "00";
 		end if;
+	else
+		r_int(31 downto 26) <= (others => '0');
+		g_int(31 downto 26) <= (others => '0');
+		b_int(31 downto 26) <= (others => '0');
 	end if;
 	if(display_enable = '1' and Hcount_int >= note6_left and Hcount_int <= note6_right) then	
 		if(Vcount_int >= block1_top + shift(15) and Vcount_int <= block1_bottom + shift(15) and init(15) = '1') then
@@ -493,6 +402,10 @@ shift_speed <= 10;
 			g_int(37 downto 36) <= "00";
 			b_int(37 downto 36) <= "00";
 		end if;
+	else
+		r_int(37 downto 32) <= (others => '0');
+		g_int(37 downto 32) <= (others => '0');
+		b_int(37 downto 32) <= (others => '0');
 	end if;
 	if(display_enable = '1' and Hcount_int >= note7_left and Hcount_int <= note7_right) then	
 		if(Vcount_int >= block1_top + shift(18) and Vcount_int <= block1_bottom + shift(18) and init(18) = '1') then
@@ -522,6 +435,10 @@ shift_speed <= 10;
 			g_int(43 downto 42) <= "00";
 			b_int(43 downto 42) <= "00";
 		end if;
+	else
+		r_int(43 downto 38) <= (others => '0');
+		g_int(43 downto 38) <= (others => '0');
+		b_int(43 downto 38) <= (others => '0');
 	end if;
 	if(display_enable = '1' and Hcount_int >= note8_left and Hcount_int <= note8_right) then	
 		if(Vcount_int >= block1_top + shift(21) and Vcount_int <= block1_bottom + shift(21) and init(21) = '1') then
@@ -551,7 +468,10 @@ shift_speed <= 10;
 			g_int(49 downto 48) <= "00";
 			b_int(49 downto 48) <= "00";
 		end if;
-	end if;
+	else
+		r_int(49 downto 44) <= (others => '0');
+		g_int(49 downto 44) <= (others => '0');
+		b_int(49 downto 44) <= (others => '0');
 	end if;
 	end process;
 	r1(1) <= r_int(1) or r_int(3) or r_int(5) or r_int(7) or r_int(9) or r_int(11) or r_int(13) or r_int(15) or r_int(17) or r_int(19) or r_int(21) or r_int(23) or r_int(25) or r_int(27) or r_int(29) or r_int(31) or r_int(33) or r_int(35) or r_int(37) or r_int(39) or r_int(41) or r_int(43) or r_int(45) or r_int(47) or r_int(49);
@@ -562,5 +482,3 @@ shift_speed <= 10;
 	g1(0) <= g_int(0) or g_int(2) or g_int(4) or g_int(6) or g_int(8) or g_int(10) or g_int(12) or g_int(14) or g_int(16) or g_int(18) or g_int(20) or g_int(22) or g_int(24) or g_int(26) or g_int(28) or g_int(30) or g_int(32) or g_int(34) or g_int(36) or g_int(38) or g_int(40) or g_int(42) or g_int(44) or g_int(46) or g_int(48);
 	b1(0) <= b_int(0) or b_int(2) or b_int(4) or b_int(6) or b_int(8) or b_int(10) or b_int(12) or b_int(14) or b_int(16) or b_int(18) or b_int(20) or b_int(22) or b_int(24) or b_int(26) or b_int(28) or b_int(30) or b_int(32) or b_int(34) or b_int(36) or b_int(38) or b_int(40) or b_int(42) or b_int(44) or b_int(46) or b_int(48);
 end behaviour;
-
-
